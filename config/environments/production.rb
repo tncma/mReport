776672c6d33d3fe38ef23494b4939f7c5e423d2d
BharathMG <mgbharath@ymail.com>
2013-12-15 11:39:40 +0530

    Mailer fixes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,4 +78,21 @@ config.assets.digest = true
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+  config.action_mailer.delivery_method = :smtp
+# Defaults to:
+# config.action_mailer.sendmail_settings = {
+#   location: '/usr/sbin/sendmail',
+#   arguments: '-i -t'
+# }
+config.action_mailer.perform_deliveries = true
+config.action_mailer.raise_delivery_errors = true
+ config.action_mailer.smtp_settings = {
+     address: "smtp.gmail.com",
+     port: 587,
+     domain: "gmail.com",
+     authentication: "plain",
+     enable_starttls_auto: true,
+     user_name: "mreportcma",
+     password: "testpassword" 
+   }
 end
