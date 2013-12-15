ce835cc3073265dfc0ddfdeb76d59e2c7c735f47
BharathMG <mgbharath@ymail.com>
2013-12-15 10:42:15 +0530

    Bundle

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,10 +27,11 @@ MReport::Application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
 
   # Generate digests for assets URLs.
-  config.assets.digest = true
+config.serve_static_assets = true
+config.assets.compile = true
+config.assets.digest = true
 
   # Version of your assets, change this if you want to expire all your assets.
   config.assets.version = '1.0'
